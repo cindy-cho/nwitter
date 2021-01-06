f3d9c0c8639ba02fd504bdb58afa07163a80768b
cindy-cho <43ryuflower@gmail.com>
2021-01-06 23:46:31 +0900

    Routes Update

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCkxJKQMp9-7bNhd1ZkurWsDiNx0eDx5rA",
+    authDomain: "twitter-d0c9f.firebaseapp.com",
+    projectId: "twitter-d0c9f",
+    storageBucket: "twitter-d0c9f.appspot.com",
+    messagingSenderId: "333817531165",
+    appId: "1:333817531165:web:0a0777452f75f7e8182ff4"
+};
+
+export default firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
+import firebase from "./firebase";
 
-import App from './App';
+console.log(firebase);
 
 
 ReactDOM.render(
