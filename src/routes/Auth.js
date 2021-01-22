0af7862747b125a1f57b5079e71060cef5c7e254
cindy-cho <43ryuflower@gmail.com>
2021-01-22 23:38:27 +0900

    LogIn Status Update

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
     const onChange = (event) => {
         const {target: {name, value}} = event;
         if(name === "email"){
@@ -29,6 +30,9 @@ const Auth = () => {
             console.log(error);
         }
     };
+
+    const toggleAccount = () => setNewAccount((prev => !prev));
+
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -49,7 +53,11 @@ const Auth = () => {
                     onChange={onChange}
                 />
                 <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                {error}
             </form>
+            <span onClick={toggleAccount}>
+                {newAccount ? "Sign In" : "Create Account"}
+            </span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
