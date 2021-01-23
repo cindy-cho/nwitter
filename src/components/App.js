575a5a5f1e52c3cebb0ce9483557b8e3dd582ec2
cindy-cho <43ryuflower@gmail.com>
2021-01-23 22:13:16 +0900

    Add LogOut Function and Nsvigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
-import { authService } from "fbase";
+import {authService} from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
